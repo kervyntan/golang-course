6bfa5937c7681e3cffdc1eccee1fdcca984aa4b6
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-12-04 22:24:56 +0800

    tips on variable declaration and value assignment in Go

--- cards/main.go
+++ cards/main.go
@@ -2,12 +2,18 @@ package main
 
 import "fmt"
 
+// below line fails
+// hey := 20
+// because Variables can be initialized outside of a function, but cannot be assigned a variable.
+
 func main() {
 	// Below 2 lines of code are EQUIVALENT
 	// second one relies on Compiler to know the type of variable
+	// type inference to a degree
 
 	// var card string = "Ace of Spades"
 	card := "Ace of Spades"
+	card = "Five of Diamonds"
 
 	fmt.Println(card)
 }
