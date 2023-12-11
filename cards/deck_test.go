971584339a8202325d35fb48b31949d2256e35d6
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-12-11 23:40:41 +0800

    Writing first test for new deck, use of standard t *testing.T argument

---
+++ cards/deck_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+/*
+Code to make sure that a deck is created with a specified
+number of cards
+*/
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	// 4 types of values, 4 suits = 4 * 4 = 16 cards
+	if len(d) != 16 {
+		t.Errorf(("Expected deck length of 16, but got %v"), len(d))
+	}
+}
